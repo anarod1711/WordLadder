0a766848e60082bed2330e4e6b98b476973ed628
Megan Nguyen <megantnguyen@utexas.edu>
2019-02-28 17:42:26 -0600

    wrote makeLinkedList dictionary and started makeGraph

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -25,7 +25,8 @@ public class Main {
 	static ArrayList <String> bfsLadder;														//
 	static Set<String> dict;																	// remove?
 	static String[] arrayDict;																	//
-	static ArrayList<LinkedList<Node>> graph = makeGraph();								  	    //
+	static ArrayList<LinkedList<Node>> graph;											  	    //
+	static LinkedList<Node> dictionaryLL; 
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -41,7 +42,6 @@ public class Main {
 			ps = System.out;			// default output to Stdout
 		}
 		initialize();
-
 		// TODO methods to read in words, output ladder
 		userInput = parse(kb);
 		
@@ -61,16 +61,34 @@ public class Main {
 		// We will call this method before running our JUNIT tests.  So call it 
 		// only once at the start of main.
 		userInput = new ArrayList<String>(); 
-		ladder = new ArrayList <String>();
-		bfsLadder = new ArrayList <String>();													//
-		dict = makeDictionary();																//
-		arrayDict = dict.toArray(new String[dict.size()]); 										//
+		ladder = new ArrayList<String>();
+		bfsLadder = new ArrayList<String>();													//
+		dictionaryLL = new LinkedList<Node>();
+		graph = new ArrayList<LinkedList<Node>>();
+		
+		
+		dictionaryLL = makeLinkedListDict();
 		graph = makeGraph();																    //		
 																							
-
-		
 	}
 	
+	public static LinkedList<Node> makeLinkedListDict() {
+		dict = makeDictionary();
+		Iterator<String> i = dict.iterator(); 
+		
+		while(i.hasNext()) {
+			Node newNode = new Node(i.next()); 
+			dictionaryLL.add(newNode); 
+		}
+//		arrayDict = dict.toArray(new String[dict.size()]); 										//
+
+		for (Node step : dictionaryLL) {
+			System.out.println(step.getData());
+		}
+		
+		return dictionaryLL; 
+		
+	}
 	/**
 	 * @param keyboard Scanner connected to System.in
 	 * @return ArrayList of Strings containing start word and end word. 
@@ -117,35 +135,37 @@ public class Main {
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
     	
-  
-		Queue <Node> myQ = new LinkedList<>(); 
-		
-		int idx = find(start);						
-		if (idx != -1) {						
-			
-			myQ.add(graph.get(idx).element());				
-			graph.get(idx).getFirst().setVisited(true);		
-			
-			while (!myQ.isEmpty()) {
-				
-				Node curr = myQ.remove();				
-				
-				if (arrayDict[curr.getData()].equals(end)) {
-					bfsLadder.add(end);
-					return bfsLadder;
-				}
-				
-				bfsLadder.add(arrayDict[curr.getData()]);
-				
-				curr = curr.getNext();
-				while (curr != null) {
-					curr.setVisited(true);
-					myQ.add(graph.get(curr.getData()).element());
-					curr = curr.getNext();
-				}
-				
-			}
-		}
+//  
+//		Queue <Node> myQ = new LinkedList<>(); 
+//		
+//		int idx = find(start);						
+//		if (idx != -1) {						
+//			
+//			myQ.add(graph.get(idx).element());				
+//			graph.get(idx).getFirst().setVisited(true);		
+//		
+//			//check if visited!
+//			
+//			while (!myQ.isEmpty()) {
+//				
+//				Node curr = myQ.remove();				
+//				
+//				if (arrayDict[curr.getData()].equals(end)) {
+//					bfsLadder.add(end);
+//					return bfsLadder;
+//				}
+//				
+//				bfsLadder.add(arrayDict[curr.getData()]);
+//				
+//				curr = curr.getNext();
+//				while (curr != null) {
+//					curr.setVisited(true);
+//					myQ.add(graph.get(curr.getData()).element());
+//					curr = curr.getNext();
+//				}
+//				
+//			}
+//		}
 		return bfsLadder; 
 	}
     
@@ -174,10 +194,38 @@ public class Main {
 	}
 																							//
 	public static ArrayList<LinkedList<Node>> makeGraph(){
-		ArrayList<LinkedList<Node>> tempGraph = new ArrayList<>();
+		String word = userInput.get(0);
+		char[] startArray = userInput.get(0).toCharArray(); 
+		int idx = 0; 
+		//add first linked list
+		graph.add(0, new LinkedList<Node>()); 
 		
+		//add start to as first node 
+		graph.get(0).add(new Node(word));
 		
+		for(int i = 0; i < userInput.get(0).length(); i++) {
+			for(char j = 'a'; j < 'z'; j++) {
+				startArray[i] = j;
+				word = startArray.toString();
+				
+				//add word to graph if it exists in dictionary
+				for(int k = 0; k < dictionaryLL.size(); k++) {
+					if(dictionaryLL.get(k).equals(word)) {
+						graph.get(k).add(new Node(word));
+					}
+				}
+								
+			}
+			idx = idx + 1; 
+			//fix how to get next word in graph to make new node
+			word = graph.get(i).get(graph.get(idx));
+			graph.add(i, new LinkedList<Node>());
+			
+		}
+		
+		return graph;
 	}
+	
 	
 
 	/* Do not modify makeDictionary */
