3af72eeb1033b4b7dd8654833ee6d128f178a78a
Analaura Rodriguez <rodrigueza934@gmail.com>
2019-03-04 12:11:42 -0600

    Replaced old node class with more useful node class. DFS, BFS done.

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -3,12 +3,12 @@
  * Replace <...> with your actual data.
  * <Student1 Name>
  * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * <16225>
+ * <Analaura Rodriguez>
+ * <ar55665>
+ * <16225>
  * Slip days used: <0>
- * Git URL:
+ * Git URL: 
  * Spring 2019
  */
 
@@ -20,15 +20,14 @@ import java.io.*;
 public class Main {
 	
 	// static variables and constants only here.
-	static ArrayList<String> userInput; 
-	static ArrayList<String> ladder;
-	static ArrayList <String> bfsLadder;
+	static ArrayList<String> userInput; 		// contains start & end word
+	static ArrayList <String> bfsLadder;	
 	static ArrayList <String> dfsLadder;
-	static Set<String> dict;																	// remove?
-	static String[] arrayDict;																	//
-	static ArrayList<LinkedList<String>> graph;											  	    //
-	static LinkedList<Node> dictionaryLL; 
-	static ArrayList<String> wordsAdded;
+	static Set<String> dict;					// instructor made dict														
+	static String[] arrayDict;					// easily accessible dict							
+	static ArrayList<LinkedList<String>> graph;	// graph based on start word										  	    
+	static ArrayList<String> wordsAdded;		// words in graph (easily accessible)
+	
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -43,51 +42,33 @@ public class Main {
 			kb = new Scanner(System.in);// default input from Stdin
 			ps = System.out;			// default output to Stdout
 		}
-		initialize();
-		// TODO methods to read in words, output ladder
+		
+		initialize();							
 		userInput = parse(kb);
-		graph = makeGraph();
-		if (!userInput.contains("/QUIT")) { // doesn't contain /quit command
-			ps.println(userInput); 
-			bfsLadder = getWordLadderBFS(userInput.get(0), userInput.get(1));					//
-			printLadder(bfsLadder);
+		
+		// IF not quit command	
+		if (!userInput.isEmpty()) { 				
+			printLadder(getWordLadderBFS(userInput.get(0), userInput.get(1)));
+			//printLadder(getWordLadderDFS(userInput.get(0), userInput.get(1)));
 		}
 		
 		//end program 
 	}
 	
+	/* Initializes static variables*/
 	public static void initialize() {
 		// initialize your static variables or constants here.
 		// We will call this method before running our JUNIT tests.  So call it 
 		// only once at the start of main.
 		userInput = new ArrayList<String>(); 
-		ladder = new ArrayList<String>();
 		bfsLadder = new ArrayList<String>();
 		dfsLadder = new ArrayList<String>();
-		dictionaryLL = new LinkedList<Node>();
 		graph = new ArrayList<LinkedList<String>>();
-		
-		wordsAdded = new ArrayList<String>();
-		dictionaryLL = makeLinkedListDict();
-																		    //		
+		wordsAdded = new ArrayList<String>();	
+		dict = makeDictionary();							
 																							
 	}
 	
-	public static LinkedList<Node> makeLinkedListDict() {
-		dict = makeDictionary();
-		Iterator<String> i = dict.iterator(); 
-		
-		while(i.hasNext()) {
-			Node newNode = new Node(i.next()); 
-			dictionaryLL.add(newNode); 
-		}
-
-		arrayDict = dict.toArray(new String[dict.size()]); 										//
-
-		
-		return dictionaryLL; 
-		
-	}
 	/**
 	 * @param keyboard Scanner connected to System.in
 	 * @return ArrayList of Strings containing start word and end word. 
@@ -95,40 +76,58 @@ public class Main {
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
 		
-		//get start word
-		String start = keyboard.next().toUpperCase();
-		userInput.add(start);
+		String start = keyboard.next().toUpperCase();	// get start word
+		 
+		if (start == "/QUIT") return userInput;			// return empty ArrayList
 		
-		// check for /quit command
-		if (!userInput.get(0).equals("/QUIT")) { 			// isn't /quit command
-			String end = keyboard.next().toUpperCase();	// get end word 
-			userInput.add(end);
-		}
+		String end = keyboard.next().toUpperCase();		// get end word 
+		
+		userInput.add(start);
+		userInput.add(end);
 
 		
-		return userInput;
+		return userInput;								// contains start & end word
 	}
 	
+	
+	/** Looks for word ladder b/w start and end word using depth 
+	 * first search, recursively.
+	 * @param String start and end word
+	 * @return if ladder exists, return ordered ladder from start to end (inclusive)
+	 * If ladder doesn't exist, return list with just start and end.
+	 */
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
+		start = start.toUpperCase();
+		end = end.toUpperCase();
 		
-		// Returned list should be ordered start to end.  Include start and end.
-		// If ladder is empty, return list with just start and end.
-
-
-		Set <String> discovered = new HashSet<>();
-		
-		if (getWordLadderDFSHelper(start, end, discovered, dfsLadder)) {
-			return dfsLadder;
+		// IF both words in dictionary, begin search
+		if (dict.contains(start) && dict.contains(end)) {
+			graph = makeGraph(start);					// make graph based on start word
+			sortGraph(graph, end);
+			
+			Set <String> discovered = new HashSet<>();	
+			
+			if (getWordLadderDFSHelper(start, end, discovered, dfsLadder)) { // populate ladder
+				return dfsLadder;
+			}
 		}
 		
+		// return w only start and end word
+		dfsLadder.clear();
 		dfsLadder.add(start);
 		dfsLadder.add(end);
-		return dfsLadder; // replace this line later with real return
+		
+		return dfsLadder; 
+		
 	}
 	
+	/** Performs recursive dfs until a path is found or not found
+	 * @param current word, end word, discovered set, ladder
+	 * @return true if ladder connection exists, false otherwise
+	 */
 	public static boolean getWordLadderDFSHelper(String current, String end, Set<String> discovered, ArrayList<String> path){
 		
-		discovered.add(current);
+		discovered.add(current); 
 		path.add(current);
 		
 		if(current.equals(end)) {
@@ -136,73 +135,124 @@ public class Main {
 		}
 		
 		else {
+			
+			// backtracking
 			for(String word : graph.get(wordsAdded.indexOf(current))) {
+				
 				if (!discovered.contains(word)) {
 					if (getWordLadderDFSHelper(word, end, discovered, path)) {
 						return true;
 					}
 				}
 			}
-			path.remove(current);
+			path.remove(current); // dead end, no path
 			return false;
 		}
 	}
 	
+	/** Looks for word ladder b/w start and end word using breadth
+	 * first search, doubly linked list.
+	 * @param Start and end word
+	 * @return IF word ladder exist, return ordered ladder from start to end (inclusive)
+	 * If ladder doesn't exist, return list with just start and end.
+	 */
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
-    	
-		Queue <String> myQ = new LinkedList<>(); 
-		Set <String> discovered = new HashSet<>();
-										
-		ArrayList<ArrayList<String>> paths;
+		start = start.toUpperCase();
+		end = end.toUpperCase();
 		
-		myQ.add(graph.get(0).getFirst());	
-		discovered.add(graph.get(0).getFirst());
-
-		
-		while (!myQ.isEmpty()) {
+    	//check word existance
+    	if (dict.contains(start) && dict.contains(end)) {
+    		graph = makeGraph(start);
+    		sortGraph(graph, end);
+    		
+	    	Queue <Node> myQ = new LinkedList<>(); 
+			Set <String> discovered = new HashSet<>();
 			
-			String curr = myQ.remove();				
+			// create start node and current node holder
+			Node startNode = new Node(start);
+			Node curr = new Node();
 			
 			
-			bfsLadder.add(curr);
+			myQ.add(startNode);								//add start node
+			discovered.add(graph.get(0).getFirst());		//add to discovered
+			boolean found = false;							//"break" out of loop when found
 			
-			boolean useful = false;
-			for (String word : graph.get(wordsAdded.indexOf(curr))) {
-				if (!discovered.contains(word)) {
-					
-					if (word.equals(end)) {
-						bfsLadder.add(end);
-						return bfsLadder;
-					}
-					
-					discovered.add(word);
-					myQ.add(word);
-					useful = true;
+			while (!myQ.isEmpty() && !found) {	
+				
+				curr = myQ.remove();						
+				
+				if (curr.getData().equals(end)) {			
+					found = true;
 				}
+				
+				if (!found) {
+					for (String word : graph.get(wordsAdded.indexOf(curr.getData()))) { // search neighbors
+						if (!discovered.contains(word)) {		
+							
+							Node newNeighbor = new Node(word);		// create nodes for neighbors 
+							newNeighbor.setParent(curr);			// set current node as parent
+							discovered.add(word);
+							myQ.add(newNeighbor);
+						}
+					}
+				}
+	
 			}
 			
-			if (!useful) bfsLadder.remove(curr);
-			
-			
-		}
-		
-		return bfsLadder; 
+			// IF found, backtrack to get path, "reverse" path
+			if (found) {
+				bfsLadder.clear();
+				Node parent = curr.getParent();
+				bfsLadder.add(curr.getData());
+				int idx = 1; // # elements in list
+				
+				while (parent != null) {
+					bfsLadder.add(bfsLadder.size() - idx, parent.getData());
+					parent = parent.getParent();
+					idx++;
+				}
+				
+				return bfsLadder;
+			}
+    	}
+    	
+		//else (not found)
+		bfsLadder.clear();
+		bfsLadder.add(start);	// add start
+		bfsLadder.add(end);		// add end
+		return bfsLadder;
 	}
     
 	
+    /** Prints ladder if exists,
+     * OR if no ladder exists, print appropriate message
+     * @param ladder
+     */
 	public static void printLadder(ArrayList<String> ladder) {
 		
-		for (String step : ladder) {
-			System.out.println(step);
+		// ladder exists
+		if (ladder.size() > 2) { 
+			System.out.println("a " + ladder.size() + "-rung word ladder exists between " + ladder.get(0).toLowerCase() + " and " + ladder.get(ladder.size() - 1).toLowerCase() + ".");
+			for (String step : ladder) {
+				System.out.println(step.toLowerCase());
+			}
+		}
+		
+		// ladder doesn't exist
+		else {
+			System.out.println("no word ladder can be found between " + ladder.get(0).toLowerCase() + " and " + ladder.get(1).toLowerCase() + ".");
 		}
 	}
 
-																							//
-	public static ArrayList<LinkedList<String>> makeGraph(){
-		
+	/** Populates a graph based on start word.Connections are 
+	 * between words that differ only by one letter. 
+	 * @param start word
+	 * @return ArrayList of linked list graph
+	 */																					
+	public static ArrayList<LinkedList<String>> makeGraph(String start){
 			
 		// get start word and convert to char array
-		String word = userInput.get(0); 
+		String word = start;
 		
 		int idx = 0; // pos to add new linked list
 		
@@ -210,15 +260,19 @@ public class Main {
 		graph.add(0, new LinkedList<String>()); 
 		graph.get(0).add(word);
 		wordsAdded.add(word);
-		Arrays.sort(arrayDict);
 		
+		// other stuff needed
+		arrayDict = dict.toArray(new String[dict.size()]); 	// transform to array (easily accessible)
+		Arrays.sort(arrayDict);									
 		
+		// populate
 		for (int LL = 0; LL < graph.size(); LL++) {
-			word = graph.get(LL).getFirst();
-			for(String dictWord : arrayDict) {
+			
+			word = graph.get(LL).getFirst();				// get head
+			for(String dictWord : arrayDict) {				// find neighbors
 				if(differByOne(word, dictWord)) {
-					graph.get(LL).addLast(dictWord); // add word to current linked list (LL)
-					if(!wordsAdded.contains(dictWord)) { //add dictWord as new head if not yet created
+					graph.get(LL).addLast(dictWord); 		// add word to current linked list (LL)
+					if(!wordsAdded.contains(dictWord)) { 	// add dictWord as new head if not in graph
 						idx++;
 						graph.add(idx, new LinkedList<String>());
 						graph.get(idx).add(dictWord);
@@ -228,9 +282,15 @@ public class Main {
 			}	
 		}
 		
+		
 		return graph;
 	}
 	
+	
+	/** checks if two words differ by one letter
+	 * @param two words to compare
+	 * @return true if words differ by one letter, false otherwise
+	 */	
 	public static boolean differByOne(String word, String dictWord) {
 		char[] wordArray = word.toCharArray();
 		char[] dictWordArray = dictWord.toCharArray();
@@ -248,7 +308,92 @@ public class Main {
 			return false;
 	
 	}
-
+	
+	/** Sorts graph by placing words that are closer 
+	 * to end word at the front of LL
+	 * @param graph
+	 */	
+	public static void sortGraph(ArrayList<LinkedList<String>> graph, String endWord) {
+		
+		
+		for (int LL = 0; LL < graph.size(); LL++) {
+			if (graph.get(LL).size() > 2) {
+				String[] wordWeight = getWeight(graph.get(LL), endWord); // orders G to L similarities w end word
+				
+				String head = graph.get(LL).getFirst(); 		// preserve head
+				graph.get(LL).clear(); 
+				graph.get(LL).add(head); 						// add same head
+				
+				for (int i = 1; i < wordWeight.length; i ++) {
+					graph.get(LL).addLast(wordWeight[i]);
+				}
+			}
+			
+		}
+		
+	}
+	
+	/** Orders linked list by placing words with more
+	 * similarities to end word at the front
+	 * @param linked list to order
+	 */	
+	public static String[] getWeight(LinkedList <String> LL, String endWord){
+		
+		// to order based on G to L similarities to end word
+		String[] wordWeight = LL.toArray(new String[LL.size()]); 
+		int[] weight = new int[LL.size()];
+		
+		
+		for (int i = 1; i < weight.length; i ++) {
+			weight[i] = countSimilarities(wordWeight[i], endWord);  // counts similarities
+		}
+		
+		// re-ordering LL from G to L similarities to end Word
+		for (int i = 2; i < weight.length; i++) {
+			for (int j = 1; j < weight.length; j++) {
+				if (weight[i] > weight[j]) {
+					
+					String temp = wordWeight[i];
+					int tempI = weight[i];
+					
+					wordWeight[i] = wordWeight[j];
+					wordWeight[j] = temp;
+					
+					weight[i] = weight[j];
+					weight[j] = tempI;
+					
+					
+				}
+			}
+		}
+		
+		
+		return wordWeight;
+	}
+	
+	
+	/** counts similar letters & position  of 
+	 * two words
+	 * @param two words to compare
+	 * @return # of in common letters
+	 */	
+	public static Integer countSimilarities(String word, String endWord) {
+		
+		char[] wordArray = word.toCharArray();
+		char[] endWordArray = endWord.toCharArray();
+		Integer count = 0;
+		
+		for(int i = 0; i < word.length(); i++) {
+			
+			if(wordArray[i] == endWordArray[i]) {
+				count++;
+			}
+			
+		}
+		
+		return count;
+	}
+	
 	/* Do not modify makeDictionary */
 	public static Set<String>  makeDictionary () {
 		Set<String> words = new HashSet<String>();
--- src/assignment3/Node.java
+++ src/assignment3/Node.java
@@ -1,34 +1,57 @@
 package assignment3;
 
 public class Node {
-	private String data;
-	private boolean visited;
-	private Node next;
+	private String data;	// word
+	private Node parent;	// neighbor
 	
-	Node(String s) {
-		data = s;
-		visited = false;
-		next = null;
+	/**
+	 * @constructor default
+	 */
+	Node() {
+		data = "";
+		parent = null;
 	}
 	
+	/**
+	 * @constructor takes data as string, 
+	 * sets parent to null
+	 */
+	Node(String s) {
+		data = s;
+		parent = null;
+	}
+	
+	/** sets node data
+	 * @param Strind data
+	 * @return none
+	 */
+	public void setData(String word) {
+		data = word;
+	}
+	
+	/** returns node dta
+	 * @param none
+	 * @return data in node
+	 */
 	public String getData() {
 		return data;
 	}
 	
-	public void setVisited(boolean discovered) {
-		visited = discovered;
+	/** sets parent of node
+	 * @param parent of node
+	 * @return none
+	 */
+	public void setParent(Node p) {
+		parent = p;
 	}
 	
-	public boolean hasVisited() {
-		return visited;
+	/** returns node parent
+	 * @param none
+	 * @return node parent
+	 */
+	public Node getParent() {
+		return(parent);
 	}
 	
-	public void setNext(Node nxt) {
-		next = nxt;
-	}
-	
-	public Node getNext() {
-		return next;
-	}
 	
 }
